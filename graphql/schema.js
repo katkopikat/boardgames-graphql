f311127a2eb9dbc8f8070197a5e3d94810c283f8
Katy Rog <katkopikat@gmail.com>
2022-04-19 20:41:06 +0300

    feat: add mutations for gamesLib

--- graphql/schema.js
+++ graphql/schema.js
@@ -7,12 +7,33 @@ const typeDefs = /* GraphQL */ `
         description: String
         maxPlayers: Int!
         minPlayers: Int!
+        score: Float
         # images: String
     }
 
+    # type UserBoardGame {
+    #     name: String!
+    #     score: Int
+    # }
+
     type Query {
-        games: [BoardGame]!
+        gamesLib: [BoardGame]!
         game(id: Int!): BoardGame
+        # userGames: [BoardGame]!
+    }
+
+    type Mutation {
+        deleteGameFromLib(id: Int): Boolean
+        addGameToLib(
+            name: String!
+            originalName: String
+            genres: [String]
+            description: String
+            maxPlayers: Int!
+            minPlayers: Int!
+            score: Int
+        ): BoardGame
+        # addUserGame(name: String, score: Int): name
     }
 `;
 
